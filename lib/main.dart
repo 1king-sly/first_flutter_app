0c99d220c276862d6b20998c13dc46e5a15d5de4
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-10-04 10:40:14 +0300

    test styling and lineargreadient for background colors

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      home: Scaffold(
-    backgroundColor: Colors.deepPurple,
-    body: Container(
-      decoration: const BoxDecoration(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
           gradient: LinearGradient(colors: [
-        Color.fromARGB(197, 141, 81, 245),
-        Color.fromARGB(200, 80, 31, 165),
-        Color.fromARGB(200, 80, 31, 165),
-      ],
-      begin:Alignment.topLeft,
-      end:Alignment.bottomRight
-      ),
-      
-      ),
-      child: const Center(
-        child: Text('Hello Byrone', style: TextStyle(
-          color: Colors.white,
-          fontSize: 28
-        ),),
+            Color.fromARGB(255, 68, 87, 255),
+            Color.fromRGBO(68, 138, 255, 1),
+            Color.fromARGB(255, 74, 68, 255),
+            Color.fromRGBO(68, 138, 255, 1),
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        ),
+        child: const Center(
+            child: Text(
+          'Hello World',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        )),
       ),
     ),
-  )));
+  ));
 }
