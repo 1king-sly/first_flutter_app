9039e43d3cf021e59662169d536f67dac3caf318
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-10-07 10:30:57 +0300

    learnt on using customizable widgets, creating widget classes and separating them on different files also using variables in styling

---
+++ lib/gradient_container.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:firstapp/styled_text.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 68, 87, 255),
+          Color.fromRGBO(68, 138, 255, 1),
+          Color.fromARGB(255, 74, 68, 255),
+          Color.fromRGBO(68, 138, 255, 1),
+        ], begin:startAlignment, end: endAlignment),
+      ),
+      child: const Center(child: StyledText()),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:firstapp/gradient_container.dart';
+
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 68, 87, 255),
-            Color.fromRGBO(68, 138, 255, 1),
-            Color.fromARGB(255, 74, 68, 255),
-            Color.fromRGBO(68, 138, 255, 1),
-          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-        ),
-        child: const Center(
-            child: Text(
-          'Hello World',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        )),
-      ),
-    ),
-  ));
+  runApp(const MaterialApp(
+      home: Scaffold(
+    body: GradientContainer(),
+  )));
 }
